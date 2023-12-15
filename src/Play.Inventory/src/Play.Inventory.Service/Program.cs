08601fd7cc6033e91081acf041a486a20b4c0fef
Stanislav Stoyanov <slavkata_99@abv.bg>
2023-12-15 15:27:57 +0200

    Add exponential backoff and circuit breaker for timeout prevention

--- src/Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -1,8 +1,11 @@
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
+var jitterer = new Random();
 
 builder.Services
     .AddMongo()
@@ -10,7 +13,17 @@ builder.Services
     .AddHttpClient<CatalogClient>(client => 
         {
             client.BaseAddress = new Uri("https://localhost:5001");
-        });
+        })
+    // Retries with exponential backoff
+    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        5,
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+    ))
+    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+        3,
+        TimeSpan.FromSeconds(15)
+    ))
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services
     .AddControllers(options =>
